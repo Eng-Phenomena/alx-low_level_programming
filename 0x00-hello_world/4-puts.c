664bf0ac5f10fb76100ea5db0accb1751f682455
Eng-Phenomena <eng.phenomena@gmail.com>
2023-07-13 10:59:42 -0700

    ch

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /*
- * main - Entry point
+ * main: Entry point
  *
  * Return: 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
