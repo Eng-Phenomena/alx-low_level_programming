a603829cc059a59c2e1c9313d842e71823017f7e
Eng-Phenomena <eng.phenomena@gmail.com>
2023-07-18 13:01:04 -0700

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - aphabit but 10x
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 'a', j = 0;
+
+	for (j = 0; j < 10; j++)
+	{
+        	for (i = 'a'; i <= 'z'; i++)
+        	{
+                	_putchar(i);
+       		}
+	}
+        _putchar('\n');	
+}
