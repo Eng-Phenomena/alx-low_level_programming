906c004136a629478cba8cd1c9e45e9dce920cb6
Eng-Phenomena <eng.phenomena@gmail.com>
2023-07-18 12:53:49 -0700

    2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase letters
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	for(int i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
