e0c422156cb8dd15ff05cd610fb354347ae7b177
Eng-Phenomena <eng.phenomena@gmail.com>
2023-07-24 10:35:28 -0700

    3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of string
+ * @s: char pointer
+ * Return: lenght
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for (i = 0; *s != '\0'; i++);
+
+	return (i);
+		
+}
