6e63b69181d813cb5bcb8524c7b2a22ab3266c48
Eng-Phenomena <eng.phenomena@gmail.com>
2023-07-24 10:55:55 -0700

    5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse
+ * @s: string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	while (*s != '\0')
+		s++;
+	s--;
+
+	while (*s != '\0')
+		_putchar(*s--);
+
+}
