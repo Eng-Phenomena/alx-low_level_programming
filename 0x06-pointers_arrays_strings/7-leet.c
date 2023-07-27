0992a175aead87c785d89596cbc7843d4071dcb8
Eng-Phenomena <eng.phenomena@gmail.com>
2023-07-27 14:02:34 -0700

    last

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * leet - mozart
+ * @c: char
+ * Return: char pointer
+ */
+
+char *leet(char *c)
+{
+	int i, j;
+	char letter[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+	
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == letter[j])
+				n[i] = num[j];
+		}
+	}
+	return (n);
+
+}
