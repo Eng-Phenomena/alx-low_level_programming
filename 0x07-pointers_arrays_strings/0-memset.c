f20ec36a675bbb7716a0501a1a7ea7c0806aa638
Eng-Phenomena <eng.phenomena@gmail.com>
2023-07-31 10:51:09 -0700

    1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * memset - fills memory with const byte
+ * @s: pointer
+ * @b: char
+ * @n: int 
+ * Return: char pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
