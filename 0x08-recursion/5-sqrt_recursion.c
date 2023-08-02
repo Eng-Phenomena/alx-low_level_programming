163745283fcc6694d7571d01ce3086c6892784b9
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-02 16:43:50 -0700

    ch

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,20 +9,20 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (sqrt(n, 0));
+	return (_sqrt(n, 0));
 }
 
 /**
- * sqrt - variable holder
+ * _sqrt - variable holder
  * @n: int
  * @i: int 
  * Return: squre root
  */
-int sqrt(int n, int i)
+int _sqrt(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (sqrt(n, i + 1));
+	return (_sqrt(n, i + 1));
 }
