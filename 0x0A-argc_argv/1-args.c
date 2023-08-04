8cd4cd3b1918a4ba4233fc3455a5474a1c6c083d
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-04 12:41:06 -0700

    sd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: counter
+ * @argv: pointer to pointer
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
