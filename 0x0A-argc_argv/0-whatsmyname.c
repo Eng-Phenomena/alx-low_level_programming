011ccfaeac000fb01a52ae1a0e6ad7b0cb2a3579
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-04 12:22:59 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: counter
+ * @argv: pointer to pointer
+ * Return: 0
+ */
+
+int main(argc, *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
