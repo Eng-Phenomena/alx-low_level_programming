b88255d0c4386119fc6d370f8f7ada9633e6a0f0
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-07 11:02:17 -0700

    1

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * alloc_grid - 2d array
+ * @width: int width
+ * @height: int height
+ * Return: 2d array pattern
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **pat, i = 0, j = 0;
+
+	if (width == 0 || height == 0)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		pat[i] = (int *) malloc(sizeof(int) * width);
+		if (pat[i] == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
+			pat[i][j] = 0;
+	}
+
+	return (pat);
+}
