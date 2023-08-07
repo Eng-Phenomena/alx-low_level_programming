ddaf8c6e71798d976e22c7677d911358ec7f5d5a
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-07 11:34:30 -0700

    1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatinating 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: Char pointer
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int count1 = 0, count2 = 0, i = 0;
+	int sum = 0;
+	char *str;
+
+	while (s1[count1] != '\0')
+		count1++;
+	while (s2[count2] != '\0')
+		count2++;
+	sum = count1 + count2;
+	str = (char*) malloc(sizeof(char) * (sum + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < count1; i++)
+		str[i] = s1[i];
+
+	for (i = 0; i < count2; i++)
+		str[i + count1] = s2[i];
+	str[sum] = '\0';
+
+	return (str);
+}
