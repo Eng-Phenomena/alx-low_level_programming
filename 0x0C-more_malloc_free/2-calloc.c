a0d9a7c1666d0e4627c710dd4af8c669383447dd
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-10 11:09:40 -0700

    3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
 
+char *setzero(char *ptr, unsigned int size);
 /**
  * _calloc - specific size array
  * @nmemb: repeated elements
@@ -11,15 +12,27 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr = malloc(size * nmemb);
-	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	if (ptr == NULL)
 		return (NULL);
+	setzero(ptr, size * nmemb);
+	return (ptr);
+}
 
-	for (i = 0; i < (size * nmemb); i++)
+/**
+ * setzero - seting zeros
+ * @ptr: pointer
+ * @size: size
+ * Return: char pointer
+ */
+
+char *setzero(char *ptr, unsigned int size)
+{
+	int i = 0;
+
+	for (i = 0; i < size; i++)
 		ptr[i] = 0;
-
 	return (ptr);
 }
