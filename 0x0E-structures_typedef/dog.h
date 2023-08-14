05563b785396aed648aef42b04d7b3aca20d1d74
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-14 09:40:22 -0700

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,9 +18,14 @@ struct dog
 };
 
 /**
- * dog_st - definition of the struct dog
+ * dog_t - definition of the struct dog
  */
 
-typedef struct dog dog_st;
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
