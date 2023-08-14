912ca3ebadaa3258821e88455f13774e99b23f7f
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-14 10:29:20 -0700

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - printing dog info
+ * @d: dog struct object
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d -> name == NULL)
+		d -> name = "(nil)";
+	if (*d.owner == NULL)
+		d -> owner = "(nil)";
+
+	printf("Name: %s\n", d -> name);
+	printf("Age: %0.6f\n", d -> age);
+	printf("Owner: %s\n", d -> owner);
+}
