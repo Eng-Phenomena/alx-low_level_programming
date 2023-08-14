b9ea62d4b2e66d425c4ca93c883324f13c378675
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-14 09:31:15 -0700

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef STRUCT_1
+#define STRUCT_1
+
+/**
+ * dog - dogs info
+ * @name: string 
+ * @age: float
+ * @owner: string
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
