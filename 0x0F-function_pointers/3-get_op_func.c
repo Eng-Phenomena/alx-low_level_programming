14d2d6e33d71299e2cbefc1a67cdc98339cd4172
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-16 05:22:07 -0700

    2

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-#include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * get_op_func - choosing opertaion to work with
@@ -10,12 +10,13 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-	{"+", op_add},
-	{"-", op_sub},
-	{"*", op_mul},
-	{"/", op_div},
-	{"%", op_mod},
-	{NULL, NULL}};
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i;
 
 	i = 0;
