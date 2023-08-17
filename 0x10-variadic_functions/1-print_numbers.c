853b6c394e98a1ad896cd996fa0206a9cf3d120c
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-17 05:59:27 -0700

    2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,6 +20,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
+		if (i == (n - 1))
+			break;
 		if (separator != NULL)
 			printf("%s", separator);
 	}
