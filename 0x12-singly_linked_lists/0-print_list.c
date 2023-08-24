6fe4176d1f169ecc0936761b6da62f658f92fabd
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-24 08:51:10 -0700

    1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints elements of list
+ * @h: srtruct pointer
+ * Return: num of lists
+ */
+
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[%d] (nil\n)", 0);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
