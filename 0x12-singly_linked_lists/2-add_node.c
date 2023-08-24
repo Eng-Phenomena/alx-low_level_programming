4d35f98273dea2849c672ec4ce767132ce3f2b28
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-24 09:19:27 -0700

    3

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adding node at beginning of list
+ * @head: pointer to pointer to the head
+ * @str: string  of struct
+ * Return: new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t new = malloc(sizeof(list_t));
+	unsigned int lenght = 0;
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+
+	while (str[lenght])
+		lenght++;
+	new->len = length;
+	new->next = (*head);
+	*head = new;
+
+	return (new);
+}
