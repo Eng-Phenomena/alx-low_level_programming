5f8240b95d3eb93e416677982fdb7b2713c60324
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-24 10:32:48 -0700

    ex

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,22 +1,15 @@
 	global main
-section .data
-    format db "Hello, Holberton", 0   ; Format string for printf
 
-section .text
-    global main                      ; Entry point for the program
-
-extern printf                        ; Declare the external printf function
+extern printf
 
 main:
-    push ebp                         ; Set up stack frame
-    mov ebp, esp
+    sub rsp, 8
 
-    push format                      ; Push the format string onto the stack
-    call printf                      ; Call the printf function
-    add esp, 4                        ; Clean up the stack after the function call
+    mov edi, message 
+    xor eax, eax 
+    call printf
 
-    mov esp, ebp                     ; Restore the stack pointer
-    pop ebp                          ; Restore the base pointer
-
-    xor eax, eax                     ; Return 0
-    ret
+    add rsp, 8
+    mov eax, 0 
+    ret
+format: db 'Hello, Holberton\n', 0
