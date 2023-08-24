f15e40ca406bf300c32ae9a425fd5740000bb19f
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-24 09:31:39 -0700

    4

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adding node at end of list
+ * @head: pointer to pointer to the head
+ * @str: string  of struct
+ * Return: new node
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+        list_t *new = malloc(sizeof(list_t));
+        unsigned int lenght = 0;
+	list_t *end = *head;
+
+	while (end)
+	{
+		end = end->next;
+	}
+        if (new == NULL)
+                return (NULL);
+
+        new->str = strdup(str);
+
+        while (str[lenght])
+                lenght++;
+        new->len = lenght;
+        new->next = NULL;
+	end->next = new;
+
+        return (new);
+}
