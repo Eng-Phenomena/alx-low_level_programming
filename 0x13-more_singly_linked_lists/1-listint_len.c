8efe8865114048013f823d4506d38053f89dc9ac
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-29 05:45:29 -0700

    2

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "list.h"
+
+/**
+ * listint_len - length of list
+ * @h: head of list
+ * Return: length
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
