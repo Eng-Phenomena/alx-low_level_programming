56356b83b03363af7ff1d8d58cdfdf969f364c54
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-29 06:58:03 -0700

    10

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - adding node at given position
+ * @head: header 
+ * @idx: position
+ * @n: value
+ * Return: address of node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *tmp = *head, *new = malloc(sizeof(listint_t));
+	unsigned int i = 0;
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	while (tmp && i < idx)
+	{
+		if (i == idx - 1)
+		{
+			new->next = tmp->next;
+			tmp->next = new;
+			return (new);
+		}
+		else
+		{
+			i++;
+			tmp = tmp->next;
+		}
+	}
+	return (NULL);
+}
