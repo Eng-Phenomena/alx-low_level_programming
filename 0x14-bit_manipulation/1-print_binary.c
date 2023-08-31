8649241802d3a44ebcfd2a00c2d0334b7d9dff9c
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-31 10:56:23 -0700

    2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,11 +8,13 @@
 
 void print_binary(unsigned long int n)
 {
+	unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
+
 	if (n == 0)
 		_putchar('0');
 	while (n)
 	{
-		_putchar((n & 1) + '0' );
+		_putchar((n & mask)? '1' : '0' );
 	       n = n >> 1;	
 	}
 }
