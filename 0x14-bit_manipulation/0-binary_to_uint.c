6c55991bf6b6346ca8129c1f37e8cd6e589c4d55
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-31 10:06:32 -0700

    1

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converting binary num to decimal
+ * @b: binary num
+ * Return: decimal value
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int d = 0, count = 0;
+	unsigned int base = 1;
+
+	if (!b)
+		return (0);
+
+	while (b[count])
+		count++;
+
+	for (; count >= 0; count--)
+	{
+		if (b[count] == '1')
+			d += base;
+		base *= 2;
+	}
+	return (d);
+}
