67595f2f647b08c44cac436b085bd809f5e8dacc
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-31 10:16:20 -0700

    2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,15 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int d = 0, count = 0;
-	unsigned int base = 1;
 
 	if (!b)
 		return (0);
 
-	while (b[count])
-		count++;
-
-	for (; count >= 0; count--)
+	for (; b[count]; count++)
 	{
-		if (b[count] < '0' || b[count] > '1')
+		if (b[count] != '0' || b[count] != '1')
 			return (0);
-
-		if (b[count] == '1')
-			d += base;
-		base *= 2;
+		d = 2 * d + (b[count] - '0');
 	}
 	return (d);
 }
