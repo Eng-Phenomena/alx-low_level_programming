491df531f1ac091c6289dc2bc4f5b853a2f10ed7
Eng-Phenomena <eng.phenomena@gmail.com>
2023-09-04 10:14:43 -0700

    1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * read_textfile - reading from a file then printing the out
+ * @filename: filename
+ * @letters: num of letters
+ * Return: 0 if error and letters if succefull
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd = open(filename, O_RDONLY);
+	ssize_t let, wri;
+	char *buf = malloc(sizeof(char) * letters);
+
+	if (filename || fd == -1)
+		return (0);
+	let = read(fd, buf, letters);
+	wri = write(1, buf, let);
+
+	return (wri);
+}
