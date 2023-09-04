38dab0df2ee5dbb2ae821a83222dae3cdac7d14d
Eng-Phenomena <eng.phenomena@gmail.com>
2023-09-04 10:47:34 -0700

    2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	int len = 0;
+	int len = 0, wri;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[len])
 			len++;
 	}
-
-	if (fd == -1 ||  write(fd, text_content, len))
+	wri = write(fd, text_content, len);
+	if (fd == -1 || wri == -1)
 		return (-1);
 	close(fd);
 
