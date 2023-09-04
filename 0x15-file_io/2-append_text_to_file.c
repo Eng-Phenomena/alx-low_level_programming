f21e5dc16c84be13a583d9a435ccf262869015f9
Eng-Phenomena <eng.phenomena@gmail.com>
2023-09-04 10:57:35 -0700

    3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,7 +12,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_WRONLY | O_CREAT | O_APPEND);
 	int len = 0, wri;
-	
+
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
@@ -24,6 +24,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1 || wri == -1)
 		return (-1);
 	close(fd);
-	
+
 	return (1);
 }
