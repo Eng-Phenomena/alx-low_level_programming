1af73f7f9baf0ababc89521ce7907d36a23275ee
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-12 04:57:56 -0700

    7

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum of all elemnts in list
+ * @head: head of list
+ * Return: sum of elemnts
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *tmp = head;
+
+	while (tmp)
+	{
+		sum += tmp->n;
+		tmp->next;
+	}
+	return (sum);
+}
