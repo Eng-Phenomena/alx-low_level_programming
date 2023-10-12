581d011ce3fd9ff74cb49fceb9e80ee116d930be
Eng-Phenomena <eng.phenomena@gmail.com>
2023-10-12 04:04:41 -0700

    2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - length of list
+ * @h: header node 
+ * Return: number of nodes in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
