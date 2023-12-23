234cbdf8a34db84de48772263f576aafa5503928
Eng-Phenomena <eng.phenomena@gmail.com>
2023-12-22 16:07:28 -0800

    fin

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
+    """
+    w = len(grid[0])
+    h = len(grid)
+    e = 0
+    size = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edge += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edge += 1
+    return size * 4 - edge * 2
